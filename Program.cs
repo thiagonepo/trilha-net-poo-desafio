7bab1ef31241f437f4feec9a0f5396f521bf91a2
Thiago Nepomuceno SIlva <thiago.silva2@terceirizadoraizen.com>
2024-10-31 11:37:21 -0300

    Realizando o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
+public class Program
+{
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+    public static void Main(string[] args)
+    {
+        // Instanciando um objeto do tipo Nokia
+        Smartphone nokia = new Nokia("12345", "Nokia 3310", "1111111111", 64);
+        Console.WriteLine("Telefone Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+
+        // Instanciando um objeto do tipo iPhone
+        Smartphone iphone = new Iphone("67890", "iPhone X", "2222222222", 128);
+        Console.WriteLine("Telefone iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
